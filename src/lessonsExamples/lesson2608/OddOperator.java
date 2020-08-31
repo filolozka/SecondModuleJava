ed63a4e3941460b0dfa5850f010346839d151cc8
filolozka <hhshkola@gmail.com>
2020-08-31 17:15:38 +0200

    added classwork of lesson 2708

---
+++ src/lessonsExamples/lesson2608/OddOperator.java
@@ -0,0 +1,10 @@
+package lessonsExamples.lesson2608;
+
+import java.util.function.UnaryOperator;
+
+public class OddOperator implements UnaryOperator<String> {
+    @Override
+    public String apply(String s) {
+        return null;
+    }
+}
